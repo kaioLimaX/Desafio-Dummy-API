8d06f07f99ef1dcbcdaf9893a8035f9ff5d1bcf4
Caio Lima <caiodelima@klever.io>
2025-12-08 21:13:14 -0300

    add mainActivity

--- app/src/main/java/com/skymobile/desafio_dummy_api/MainActivity.kt
+++ app/src/main/java/com/skymobile/desafio_dummy_api/MainActivity.kt
@@ -1,20 +1,35 @@
 package com.skymobile.desafio_dummy_api
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.LayoutInflater
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.skymobile.desafio_dummy_api.databinding.ActivityMainBinding
+import com.skymobile.desafio_dummy_api.view.activity.products.ProductsActivity
 
 class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(binding.root) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        //navegação para tela de produtos
+        binding.btnProdutos.setOnClickListener {
+            val intent = Intent(this, ProductsActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
